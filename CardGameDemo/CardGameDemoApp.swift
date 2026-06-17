809ed6f8619bfb389b3289872a1a95d5ad908a3a
AndrewJuers <juersandrew@gmail.com>
2026-06-17 13:22:09 -0400

    Initial Commit

---
+++ CardGameDemo/CardGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameDemoApp.swift
+//  CardGameDemo
+//
+//  Created by Andrew   Juers on 6/17/26.
+//
+
+import SwiftUI
+
+@main
+struct CardGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
